1ca0bc4df2a961c2067cb49104c72a4598636e00
Dennis Vash <dennisvash@gmail.com>
2020-12-29 02:13:31 -0800

    Update urls.js

--- src/config/urls.js
+++ src/config/urls.js
@@ -5,7 +5,7 @@ const { CV, GITHUB, GMAIL, SANDBOX, SO, TWITTER } = SOCIALS;
 const socialUrls = {
   [GITHUB]: `https://github.com/denvash`,
   [TWITTER]: `https://twitter.com/dennisvash`,
-  [GMAIL]: `mailto:dennisvash@gmail.com`,
+  [GMAIL]: `mailto:dennisvash@hey.com`,
   [CV]: `/resume.pdf`,
   [SANDBOX]: `https://codesandbox.io/u/denvash`,
   [SO]: `https://stackoverflow.com/users/7882470/dennis-vash`,
